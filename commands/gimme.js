919a5e3bd568565ac3eb95faea1f6f1f62deeb42
Ubuntu <ubuntu@ip-172-31-35-252.eu-west-1.compute.internal>
2021-03-19 07:46:52 +0000

    add error message on gimme

--- commands/gimme.js
+++ commands/gimme.js
@@ -17,20 +17,30 @@ exports.run = async (client, message, [key, ...args], level) => { // eslint-disa
   }) ;
   /**/
   if (key === "allItems") {
-    const guild_id = message.guild.id, owner_id = message.author.id ;
-    var [rows, fields] = await client.connection.promise().query ("select id as characterId from wanshitong.`character` where guild_id=? and is_available=1;", [guild_id]) ;
-    rows.forEach (async (row) => {
-      let [rowsI, fieldsI] = await client.connection.promise().query ("select id as itemId from wanshitong.item where guild_id=? and character_id=? ;", [guild_id, row.characterId]) ;
-      rowsI.forEach (rowI => {
-        client.connection.execute ("insert into wanshitong.inventory (owner_id, item_id, guild_id) values (?, ?, ?) on duplicate key update item_id=item_id ;", [owner_id, rowI.itemId, guild_id], (err, res, fields) => {
-          console.error (err) ; console.log (res) ;
+    try {
+      const guild_id = message.guild.id, owner_id = message.author.id ;
+      var [rows, fields] = await client.connection.promise().query ("select id as characterId from wanshitong.`character` where guild_id=? and is_available=1;", [guild_id]) ;
+      rows.forEach (async (row) => {
+        let [rowsI, fieldsI] = await client.connection.promise().query ("select id as itemId from wanshitong.item where guild_id=? and character_id=? ;", [guild_id, row.characterId]) ;
+        rowsI.forEach (rowI => {
+          client.connection.execute ("insert into wanshitong.inventory (owner_id, item_id, guild_id) values (?, ?, ?) on duplicate key update item_id=item_id ;", [owner_id, rowI.itemId, guild_id], (err, res, fields) => {
+            console.error (err) ; console.log (res) ;
+          })
         })
-      })
-    }) ;
-    
+      }) ;
+      message.reply (`all items given to ${message.member.displayName} ;`) ;
+    } catch (err) {
+      console.log ("err gimme allItems:", err) ;
+      message.reply ("an error occured !") ;
+    }
   } else {
-    const guildConf = client.settings.get (message.guild.id) ;
-    await client.dropCharacter (message.channel, guildConf) ;
+    try {
+      const guildConf = client.settings.get (message.guild.id) ;
+      await client.dropCharacter (message.channel, guildConf) ;
+    } catch (err) {
+      console.log ("err gimme:", err) ;
+      message.reply ("an error occured !") ;
+    }
   }
 };
 
