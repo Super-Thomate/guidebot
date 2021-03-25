d3bad4e636473ab5eb8b5b7d42d5fe598531a5ec
Ubuntu <ubuntu@ip-172-31-35-252.eu-west-1.compute.internal>
2021-03-25 11:13:32 +0000

    add more info on character

--- commands/game.js
+++ commands/game.js
@@ -19,12 +19,12 @@ exports.run = async (client, message, [action, id, ...value], level) => { // esl
   if (action === "show") {
     if (!id) return message.reply("Please specify a characterId to show.");
     try {
-      var [rows, fields] = await client.connection.promise().query("select A.id as characterId, A.name as characterName, A.image, A.is_available, B.id as itemId, B.name as itemName, B.rarity as itemRarity from `character` as A, `item` as B where A.id=B.character_id and A.id=? ;", [id]) ;
+      var [rows, fields] = await client.connection.promise().query("select A.id as characterId, A.name as characterName, A.rarity as characterRarity, A.image, A.is_available, B.id as itemId, B.name as itemName, B.rarity as itemRarity from `character` as A, `item` as B where A.id=B.character_id and A.id=? ;", [id]) ;
       if (! rows.length) return message.reply (`No character with id ${id}.`) ;
       //console.log (rows) ;
       var characterEmbed = new client.Discord.MessageEmbed()
                              .setColor("#DDA624")
-                             .setTitle(`${rows [0].is_available ? ":white_check_mark:":":x:"} ${rows [0].characterName}`)
+                             .setTitle(`${rows [0].is_available ? ":white_check_mark:":":x:"} ${rows [0].characterName} [${client.getRarityCharacter (rows[0].characterRarity)}]`)
                              .setImage(rows [0].image)
                              ;
       rows.forEach(row => {
