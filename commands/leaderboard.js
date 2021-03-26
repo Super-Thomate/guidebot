dde34b72cfbb45114f91c63442502f7eb1a7b47c
Ubuntu <ubuntu@ip-172-31-35-252.eu-west-1.compute.internal>
2021-03-26 07:55:10 +0000

    what about user not found on lb

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -81,24 +81,26 @@ async function getBody (client, guild, maxLength, message, page, maxPerPage) {
   const limit = (page-1)*maxPerPage ;
   let [rows, fields] = await client.connection.promise().query ("select user_id, items, complete from `wanshitong`.`gamelb` where guild_id=? order by items desc, date_completed asc limit "+limit+", "+maxPerPage+" ;", [guild.id]) ;
   var ranking = (page-1)*maxPerPage ;
-  rows.forEach ( async (row) => {
+  rows.forEach ( (row) => {
     ranking++ ;
-    //let guildMember = guild.members.cache.find(user => user.id === row.user_id) ;
-    let guildMember = await guild.members.fetch (row.user_id.toString()) ;
+    let guildMember = guild.members.cache.find(user => user.id === row.user_id) ;
     if (! guildMember) {
       console.log (`GuildMember not found in cache with user_id = ${row.user_id}`) ;
       guildMember = {"displayName": "NONAME", "user":{"tag": "NOTAG"}} ;
-      /*
+      /**/
       guild.members.fetch (row.user_id.toString()).then (member => {
         console.log (`fetch ('${row.user_id}') => `, member) ;
         console.log (`displayName ${member.displayName}, tag ${member.user.tag} `) ;
+        guildMember = member ;
       }).catch (console.error);
-      */
+      /**/
+      /*
       try {
         guildMember = await guild.members.fetch (row.user_id.toString()) ;
       } catch (err) {
         console.error (err) ;
       }
+      */
     }
     let items = `${row.items}` ;
     let complete = row.items>=client.maxItem || row.complete;
