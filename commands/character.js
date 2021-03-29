a52c908ef4a326d540444bc9aa214a6ee4fabde3
Ubuntu <ubuntu@ip-172-31-35-252.eu-west-1.compute.internal>
2021-03-29 12:55:15 +0000

    command to handle character

---
+++ commands/character.js
@@ -0,0 +1,65 @@
+exports.run = async (client, message, [action, id, key, ...value], level) => { // eslint-disable-line no-unused-vars
+  // Retrieve current guild settings (merged) and overrides only
+  const settings = message.settings;
+  const defaults = client.settings.get("default");
+  const overrides = client.settings.get(message.guild.id);
+  if (!client.settings.has(message.guild.id)) client.settings.set(message.guild.id, {});
+  
+  // Edit an existing character
+  if (action === "edit") {
+    // User must specify an id.
+    if (!id) return message.reply("Please specify a characterId to edit.");
+    if (!key) return message.reply ("Please specify a key to edit.") ;
+    switch (key) {
+      case 'available':
+      case 'dispo':
+        let is_available = value.join('').toLowerCase() === "true" ? 1 : 0 ;
+        try {
+         await client.connection.promise().query("update `character` set is_available=? where id=? ;", [is_available, id]) ;
+         return message.reply (`key is_available is now ${is_available}.`)
+        } catch (err) {
+          console.error ("err on edit character:", err) ;
+          return message.reply ("An error occured.")
+        }
+      break ;
+      default:
+        return message.reply (`Key ${key} is unknown.`)
+    }
+  } else
+  if (action === "show") {
+    if (!id) return message.reply("Please specify a characterId to show.");
+    try {
+      var [rows, fields] = await client.connection.promise().query("select A.id as characterId, A.name as characterName, A.rarity as characterRarity, A.image, A.is_available, B.id as itemId, B.name as itemName, B.rarity as itemRarity from `character` as A, `item` as B where A.id=B.character_id and A.id=? ;", [id]) ;
+      if (! rows.length) return message.reply (`No character with id ${id}.`) ;
+      //console.log (rows) ;
+      var characterEmbed = new client.Discord.MessageEmbed()
+                             .setColor("#DDA624")
+                             .setTitle(`${rows [0].is_available ? ":white_check_mark:":":x:"} ${rows [0].characterName} [${client.getRarityCharacter (rows[0].characterRarity)}]`)
+                             .setImage(rows [0].image)
+                             ;
+      rows.forEach(row => {
+        characterEmbed.addField (`${client.getRarityEmoji (row.itemRarity)} ${client.getRarityItem (row.itemRarity)}`, `#${row.itemId} ${row.itemName.upperCaseFirstLetter()}`)
+      }) ;
+      message.channel.send (characterEmbed) ;
+    } catch (err) {
+      console.error ("error on game show characterId", err) ;
+      message.reply ("An error occured.") ;
+    }
+  } else {
+    message.reply (`${action} is not a valid action [edit,show].`) ;
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: "Moderator"
+};
+
+exports.help = {
+  name: "character",
+  category: "Game Settings",
+  description: "View or change information for a character.",
+  usage: "character <edit/show> <characterId>"
+};
