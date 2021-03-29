7471a12682a1f027354e3b6f2f9eb52a0244ece0
Ubuntu <ubuntu@ip-172-31-35-252.eu-west-1.compute.internal>
2021-03-29 12:20:46 +0000

    better user detection

--- commands/gameclear.js
+++ commands/gameclear.js
@@ -1,13 +1,13 @@
 exports.run = async (client, message, [...user], level) => { // eslint-disable-line no-unused-vars
   const userJoin = user.join (' ') ;
-  const member = message.guild.members.cache.find (u => u.id === userJoin || u.displayName === userJoin || u.user.tag === userJoin) ;
+  const member = message.guild.members.cache.find (u => u.id === userJoin || u.displayName === userJoin || (userJoin.match(/^<@!?(\d+)>$/) && u.id === userJoin.match(/^<@!?(\d+)>$/)[1]) || u.user.tag === userJoin || u.user.username === userJoin ) ;
   if (user.length) {
     if (typeof member === "undefined" || member === null) return message.reply (`${userJoin} is not a valid a user.`) ;
     try {
       await client.connection.promise().execute ("delete from wanshitong.inventory where owner_id=? and guild_id=? ;", [member.id, message.guild.id]) ;
       await client.connection.promise().execute ("delete from wanshitong.inventory_event where owner_id=? and guild_id=? ;", [member.id, message.guild.id]) ;
       await client.connection.promise().execute ("delete from wanshitong.gamelb where user_id=? and guild_id=? ;", [member.id, message.guild.id]) ;
-      message.reply (`je supprime les datas de l'user ${userJoin}`) ;
+      message.reply (`je supprime les datas de l'user ${userJoin} (${member.user.tag})`) ;
     } catch (err) {
       console.error ("err clear for user:", err) ;
       message.reply (`an error occured.`) ;
