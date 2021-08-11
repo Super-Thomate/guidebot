baa8d5233f193e44fc2070284f0422ff26624d5b
Thomas Plaindoux <t.plaindoux@cartoon-fantasy.net>
2021-08-11 20:19:40 +0200

    fix db

--- db.js
+++ db.js
@@ -15,10 +15,12 @@ connection.execute ("TRUNCATE TABLE `wanshitong`.`item` ;", (err, rows) => {
   console.log ("err truncate item:",err) ;
   // console.log ("rows:", rows) ;
 }) ;
+/*
 connection.execute ("drop table `character`;", (err, res) => {console.error ("err drop character", err) ;}) ;
 connection.execute ("CREATE TABLE IF NOT EXISTS `character` (`id` INT NOT NULL AUTO_INCREMENT, `serie` VARCHAR(128) NOT NULL, `name` VARCHAR(256) NOT NULL, `image` VARCHAR(256) NOT NULL, `rarity` INT NOT NULL, primary key (`id`)) ;", (err, res) => {console.error ("err create character", err) ;}) ;
 connection.execute ("drop table `availability`;", (err, res) => {console.error ("err drop availability", err) ;}) ;
 connection.execute ("CREATE TABLE IF NOT EXISTS `availability` (`character_id` INT NOT NULL, `guild_id` BIGINT NOT NULL, `is_available` SMALLINT NOT NULL DEFAULT 0, primary key (`character_id`, `guild_id`)) ;", (err, res) => {console.error ("err create availablity", err) ;}) ;
+*/
 //throw '' ;
 // 494812563016777729
 /*
@@ -72,7 +74,7 @@ fs.createReadStream('wst.csv')
     console.log('CSV file successfully processed');
   });
 
-
+/*
 fs.createReadStream('wst_avril.csv')
   .pipe(csv())
   .on('data', (row) => {
@@ -93,14 +95,14 @@ fs.createReadStream('wst_avril.csv')
          //console.log (res) ;
           if (err) return console.error ("err character "+current, err) ;
          const character_id = res.insertId ;
-         /*
+         //
          console.log ("itemCommon:", itemCommon)
          console.log ("itemUncommon:", itemUncommon)
          console.log ("itemRare:", itemRare)
          console.log ("itemEpic:", itemEpic)
          console.log ("character_id:", character_id)
          console.log ("guild_id:", guild_id)
-         */
+         //
          connection.execute ("insert into wanshitong.`item` (name, rarity, character_id) values (?, 1, ?) ;", [itemCommon, character_id], (err, res) => {console.error ("err common "+current, err) ;}) ;
          connection.execute ("insert into wanshitong.`item` (name, rarity, character_id) values (?, 2, ?) ;", [itemUncommon, character_id], (err, res) => {console.error ("err unco "+current, err) ;}) ;
          connection.execute ("insert into wanshitong.`item` (name, rarity, character_id) values (?, 3, ?) ;", [itemRare, character_id], (err, res) => {console.error ("err rare "+current, err) ;}) ;
@@ -110,6 +112,7 @@ fs.createReadStream('wst_avril.csv')
   .on('end', () => {
     console.log('CSV file successfully processed');
   });
+  */
   
 function getRarityCharacter (rarity) {
   return ["high", "regular", "low", "event"].indexOf (rarity)+1 ;
